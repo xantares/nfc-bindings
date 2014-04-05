b0712b98e83e59e74a9e47f9dd483ae309de25b0
xantares <xantares09@hotmail.com>
2014-04-05 11:56:57 +0200

    enhanced find script

--- cmake/FindLibNFC.cmake
+++ cmake/FindLibNFC.cmake
@@ -1,90 +1,86 @@
-# - Find LibNFC
-# LibNFC is an extensible high performance math expression parser library written in C++
-# http://muparser.sourceforge.net
+#.rst:
+# LibNFC
+# --------
 #
-# The module defines the following variables:
-#  LIBNFC_FOUND        - True if LibNFC found.
-#  LIBNFC_INCLUDE_DIRS - where to find nfc/nfc.h, etc.
-#  LIBNFC_LIBRARIES    - List of libraries when using LibNFC.
+# Find libnfc
 #
+# Find libnfc includes and library. Once done this will define
+#
+# ::
+#
+#   LIBNFC_INCLUDE_DIRS   - where to find nfc/nfc.h, etc.
+#   LIBNFC_LIBRARIES      - List of libraries when using libnfc.
+#   LIBNFC_FOUND          - True if libnfc found.
+#
+#
+#
+# ::
+#
+#   LIBNFC_VERSION_STRING - The version of libnfc found (x.y.z)
+#   LIBNFC_VERSION_MAJOR  - The major version of libnfc
+#   LIBNFC_VERSION_MINOR  - The minor version of libnfc
+#   LIBNFC_VERSION_PATCH  - The patch version of libnfc
+#
+#
+#=============================================================================
+# Copyright 2011-2014 Kitware, Inc.
+#
+# Distributed under the OSI-approved BSD License (the "License");
+# see accompanying file Copyright.txt for details.
+#
+# This software is distributed WITHOUT ANY WARRANTY; without even the
+# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the License for more information.
+#=============================================================================
+# (To distribute this file outside of CMake, substitute the full
+#  License text for the above reference.)
+
 
 if ( LIBNFC_INCLUDE_DIR AND LIBNFC_LIBRARIES )
   # Already in cache, be silent
   set ( LibNFC_FIND_QUIETLY TRUE )
 endif ()
 
+find_package(PkgConfig)
+pkg_check_modules(PC_LIBNFC libnfc)
+
+
 find_path ( LIBNFC_INCLUDE_DIR
             NAMES
             nfc/nfc.h
+            PATHS ${PC_LIBNFC_INCLUDEDIR}
           )
-            
-set ( LIBNFC_INCLUDE_DIRS ${LIBNFC_INCLUDE_DIR} )
 
 # version
-set ( _VERSION_FILE ${LIBNFC_INCLUDE_DIR}/LibNFCDef.h )
-if ( EXISTS ${_VERSION_FILE} )
-  file ( STRINGS ${_VERSION_FILE} _LIBNFC_VERSION_LINE REGEX "#define[ ]+MUP_VERSION[ ]+_T\\(\".*\"\\)" )
-  if ( _LIBNFC_VERSION_LINE )
-    string ( REGEX REPLACE ".*_T\\(\"(.*)\"\\)" "\\1" LIBNFC_VERSION_STRING ${_LIBNFC_VERSION_LINE} )
-    if ( LIBNFC_VERSION_STRING )
-      string ( REGEX REPLACE "([0-9]+)\\..*" "\\1" LIBNFC_MAJOR_VERSION ${LIBNFC_VERSION_STRING} )
-      string ( REGEX REPLACE "[0-9]+\\.([0-9]+).*" "\\1" LIBNFC_MINOR_VERSION ${LIBNFC_VERSION_STRING} )
-      string ( REGEX REPLACE "[0-9]+\\.[0-9]+\\.([0-9]+).*" "\\1" LIBNFC_PATCH_VERSION ${LIBNFC_VERSION_STRING} )
-      if ( LIBNFC_PATCH_VERSION STREQUAL LIBNFC_VERSION_STRING )
-        set ( LIBNFC_PATCH_VERSION )
-      endif ()
-    endif ()
+set ( LIBNFC_VERSION_STRING ${PC_LIBNFC_VERSION} )
+if ( LIBNFC_VERSION_STRING )
+  string ( REGEX REPLACE "([0-9]+)\\..*" "\\1" LIBNFC_MAJOR_VERSION ${LIBNFC_VERSION_STRING} )
+  string ( REGEX REPLACE "[0-9]+\\.([0-9]+).*" "\\1" LIBNFC_MINOR_VERSION ${LIBNFC_VERSION_STRING} )
+  string ( REGEX REPLACE "[0-9]+\\.[0-9]+\\.([0-9]+).*" "\\1" LIBNFC_PATCH_VERSION ${LIBNFC_VERSION_STRING} )
+  if ( LIBNFC_PATCH_VERSION STREQUAL LIBNFC_VERSION_STRING )
+    set ( LIBNFC_PATCH_VERSION )
   endif ()
 endif ()
 
-# check version
-set ( _LIBNFC_VERSION_MATCH TRUE )
-if ( LibNFC_FIND_VERSION AND LIBNFC_VERSION_STRING )
-  if ( LibNFC_FIND_VERSION_EXACT )
-    if ( ${LibNFC_FIND_VERSION} VERSION_EQUAL ${LIBNFC_VERSION_STRING} )
-    else()
-      set ( _LIBNFC_VERSION_MATCH FALSE)
-    endif ()
-  else ()
-    if ( ${LibNFC_FIND_VERSION} VERSION_GREATER ${LIBNFC_VERSION_STRING} )
-      set ( _LIBNFC_VERSION_MATCH FALSE )
-    endif ()
-  endif ()
-endif ()
 
 find_library ( LIBNFC_LIBRARY
                NAMES nfc
+               PATHS ${PC_LIBNFC_LIBDIR}
                )
                
-# set LIBNFC_LIBRARIES
-set ( LIBNFC_LIBRARIES ${LIBNFC_LIBRARY} )
-list (APPEND LIBNFC_LIBRARIES usb)
-
-# root dir
-# try to guess root dir from include dir
-if ( LIBNFC_INCLUDE_DIR )
-  string ( REGEX REPLACE "(.*)/include.*" "\\1" LIBNFC_ROOT_DIR ${LIBNFC_INCLUDE_DIR} )
-
-# try to guess root dir from library dir
-elseif ( LIBNFC_LIBRARY )
-  string ( REGEX REPLACE "(.*)/lib[/|32|64].*" "\\1" LIBNFC_ROOT_DIR ${LIBNFC_LIBRARY} )
-endif ()
-
 # handle REQUIRED and QUIET options
 include ( FindPackageHandleStandardArgs )
+# include(${CMAKE_CURRENT_LIST_DIR}/FindPackageHandleStandardArgs.cmake)
+find_package_handle_standard_args ( LibNFC 
+  REQUIRED_VARS LIBNFC_LIBRARY LIBNFC_INCLUDE_DIR
+  VERSION_VAR LIBNFC_VERSION_STRING
+)
 
-if ( CMAKE_VERSION VERSION_LESS 2.8.3 )
-  find_package_handle_standard_args ( LibNFC DEFAULT_MSG LIBNFC_LIBRARY
-    _LIBNFC_VERSION_MATCH
-    LIBNFC_LIBRARIES
-    LIBNFC_INCLUDE_DIR
-    LIBNFC_VERSION_STRING
-  )
-else ()
-  find_package_handle_standard_args ( LibNFC 
-    REQUIRED_VARS LIBNFC_LIBRARY _LIBNFC_VERSION_MATCH LIBNFC_LIBRARIES LIBNFC_INCLUDE_DIR
-    VERSION_VAR LIBNFC_VERSION_STRING
-  )
+if (LIBNFC_FOUND)          
+  set ( LIBNFC_INCLUDE_DIRS ${LIBNFC_INCLUDE_DIR} )
+  set ( LIBNFC_LIBRARIES ${LIBNFC_LIBRARY} )
+  list (APPEND LIBNFC_LIBRARIES usb)
+  string ( REGEX REPLACE "(.*)/include.*" "\\1" LIBNFC_ROOT_DIR ${LIBNFC_INCLUDE_DIR} )
 endif ()
 
 mark_as_advanced (
